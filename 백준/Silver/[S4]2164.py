9759eac861f266dd79effe8f76d0822acf83ad6f
Hyeonseo-Choi <chs926@naver.com>
2024-01-15 22:37:47 +0900

    2164. 카드2

---
+++ 백준/Silver/[S4]2164.py
@@ -0,0 +1,12 @@
+from collections import deque
+que = deque()
+n = int(input())
+
+for i in range(1, n+1):
+    que.append(i)
+
+while len(que) > 1:
+    que.popleft()
+    que.append(que.popleft())
+
+print(que.popleft())
